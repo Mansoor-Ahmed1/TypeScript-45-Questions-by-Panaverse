fec6b0e1187d46b1e439b52e71f6e02456a957a9
Mansoor Ahmed <mansoor.ahmed11521@gmail.com>
2024-05-27 01:07:00 +0500

    pushing Q21, Q22, Q23

---
+++ Q22/main.js
@@ -0,0 +1,7 @@
+//Intentional Error: If you haven’t received an array index error in one of your programs yet, try to make 
+//one happen. Change an index in one of your programs to produce an index error. Make sure you correct the 
+//error before closing the program.
+var numArray = [42, 21, 3, 14, 592, 71];
+//             indexes =   0  1 2 3   4   5
+console.log(numArray[23]); // max index can be 5
+console.log(numArray[-1]); // no element can haave negative index
---
+++ Q23/main.ts
@@ -0,0 +1,22 @@
+// Conditional Tests: Write a series of conditional tests. Print a statement describing each test and your 
+//prediction for the results of each test. Your code should look something like this:
+
+// let car = 'subaru';
+// console.log("Is car == 'subaru'? I predict True.")
+// console.log(car == 'subaru')
+
+//• Create at least 10 tests. Have at least 5 tests evaluate to True and another 5 tests evaluate to False.
+
+let car: string = "Audi"
+
+console.log(car == "Audi") //true
+console.log(car !== "Mercedez") //true
+console.log(car !== "Toyota") //true
+console.log(car !== "Honda") //true
+console.log(car !== "Subaru") //true
+
+console.log(car !== "Audi") //false
+console.log(car == "Mercedez") //false
+console.log(car == "Toyota") //false
+console.log(car == "Honda") //false
+console.log(car == "Subaru") //false
